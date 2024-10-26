4024141e9605874d1b3f379418b473aecd0179de
hafsa <hafsaraii>
2024-10-27 00:19:03 +0200

    first commit

--- front/src/app/admin-dashboard/admin-dashboard.component.ts
+++ front/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormContainerService } from '../services/form-container.service';
+
+interface FormContainer {
+  id: number;
+  title: string;
+  userEmail: string;
+  managerEmail: string;
+  ticket: string;
+  escalation: boolean;
+  validated: boolean;
+  forms: Form[];
+}
+
+interface Form {
+  id: number;
+  title: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.scss'
+  styleUrls: ['./admin-dashboard.component.scss']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  formContainers: FormContainer[] = [];
 
+  constructor(private formContainerService: FormContainerService) {}
+
+  ngOnInit(): void {
+    this.loadFormContainers();
+  }
+
+  loadFormContainers() {
+    this.formContainerService.getFormContainers().subscribe((containers: FormContainer[]) => {
+      this.formContainers = containers;
+    });
+  }
+
+  validateContainer(containerId: number) {
+    this.formContainerService.validateContainer(containerId).subscribe(() => {
+      this.loadFormContainers();
+    });
+  }
+
+  addFormToContainer(containerId: number) {
+    this.formContainerService.addFormToContainer(containerId).subscribe(() => {
+      this.loadFormContainers();
+    });
+  }
 }
