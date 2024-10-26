5054d34ac9ea3a7d67697c127cb0b299d2d46b5d
hafsa <hafsaraii>
2024-10-26 18:17:23 +0200

    first commit

--- backend/auth.py
+++ backend/auth.py
@@ -1,14 +1,23 @@
 # auth.py
 from authlib.integrations.flask_client import OAuth
 from flask import Blueprint, redirect, url_for, session
-from models import db, SuperAdmin
+from models import db
 
 oauth = OAuth()
 
 auth_bp = Blueprint('auth', __name__)
 
 # Configurez le fournisseur OAuth ici
-oauth.init_app(auth_bp)
+def setup_oauth(app):
+    oauth.init_app(app)
+    oauth.register(
+        name='google',
+        client_id='YOUR_CLIENT_ID',
+        client_secret='YOUR_CLIENT_SECRET',
+        access_token_url='https://accounts.google.com/o/oauth2/token',
+        authorize_url='https://accounts.google.com/o/oauth2/auth',
+        client_kwargs={'scope': 'openid email profile'},
+    )
 
 
 @auth_bp.route('/login')
@@ -22,7 +31,7 @@ def auth_callback():
     token = oauth.provider.authorize_access_token()
     user_info = oauth.provider.userinfo()
 
-    super_admin = SuperAdmin.query.filter_by(email=user_info['email']).first()
+    super_admin = True
     if not super_admin:
         return "Accès refusé : Vous n'êtes pas autorisé", 403
 
