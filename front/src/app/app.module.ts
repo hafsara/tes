6af1a51d82ef0f1fa71a8827a915b95492ac4b8a
root <hafsa.raii@um5s.net.ma>
2024-10-26 22:17:52 +0000

    test

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms'; // <-- Ajoutez ceci
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateFormComponent } from './create-form/create-form.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateFormComponent
+    CreateFormComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,
