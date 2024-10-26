193268c150f789892c0a48d3b670d09398849d3e
hafsa <hafsaraii>
2024-10-26 20:35:12 +0200

    first commit

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateFormComponent } from './create-form/create-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/create-form', pathMatch: 'full' },
+  { path: 'create-form', component: CreateFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
