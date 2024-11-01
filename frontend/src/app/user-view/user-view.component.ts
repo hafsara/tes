ef3b40d76547744dc070a4ff96b2f8395cd2b8a5
hafsa <hafsaraii>
2024-11-01 13:41:00 +0100

    truc qui marche mais pas bien

--- frontend/src/app/user-view/user-view.component.ts
+++ frontend/src/app/user-view/user-view.component.ts
@@ -32,7 +32,27 @@ export class UserViewComponent implements OnInit {
       if (question.type === 'dropdown') {
         this.questionDropdownOptions[question.text] = question.options.map(opt => ({ label: opt, value: opt }));
       }
+
+      if (question.type === 'checkbox') {
+        // Initialize selectedOptions as an empty array if undefined
+        question.selectedOptions = question.selectedOptions || [];
+      }
     });
+  }
+
+  // Check if an option is selected
+  isChecked(selectedOptions: string[], option: string): boolean {
+    return selectedOptions.includes(option);
+  }
+
+  // Toggle the selection of an option in the selectedOptions array
+  toggleOption(selectedOptions: string[], option: string) {
+    const index = selectedOptions.indexOf(option);
+    if (index === -1) {
+      selectedOptions.push(option);
+    } else {
+      selectedOptions.splice(index, 1);
+    }
   }
 
   onSubmit() {
