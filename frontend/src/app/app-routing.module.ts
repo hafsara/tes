8757d0931c5efe4a6b375491950d72d7a75e48ce
hafsa <hafsaraii>
2024-11-01 18:15:39 +0100

    costemics

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateFormComponent } from './create-form/create-form.component';
 import { UserViewComponent } from './user-view/user-view.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/user-view', pathMatch: 'full' },
-  { path: 'user-view', component: UserViewComponent }
+  { path: 'create-form', component: CreateFormComponent },
+  { path: 'user-view', component: UserViewComponent },
+  { path: '', redirectTo: '/create-form', pathMatch: 'full' }, // Default route
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,60 +1,44 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';  // Required for ngModel
+import { AppRoutingModule } from './app-routing.module'; // Ensure your routing module is imported
 
-// PrimeNG Modules
-import { StepsModule } from 'primeng/steps';
+// PrimeNG component imports
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
 import { DropdownModule } from 'primeng/dropdown';
-import { ToggleButtonModule } from 'primeng/togglebutton';
-import { TooltipModule } from 'primeng/tooltip';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { RadioButtonModule } from 'primeng/radiobutton';
 import { StepperModule } from 'primeng/stepper';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputNumberModule } from 'primeng/inputnumber';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { CheckboxModule } from 'primeng/checkbox';
-import { CardModule } from 'primeng/card';
 
+import { AppComponent } from './app.component';
+import { CreateFormComponent } from './create-form/create-form.component';
 import { UserViewComponent } from './user-view/user-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CreateFormComponent,
     UserViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    // PrimeNG Modules
-    StepsModule,
-    InputNumberModule,
+    AppRoutingModule,
     ButtonModule,
-    InputTextModule,
-    RadioButtonModule,
+    CardModule,
     CheckboxModule,
-    FloatLabelModule,
-    InputTextareaModule,
     DropdownModule,
-    ToggleButtonModule,
-    TooltipModule,
+    InputTextareaModule,
+    RadioButtonModule,
     StepperModule,
-    IconFieldModule,
-    InputIconModule,
-    CardModule
+    FloatLabelModule,
+    InputNumberModule,
   ],
-  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
