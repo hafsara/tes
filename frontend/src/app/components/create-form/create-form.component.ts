fbccf0a78466017a3b0ab171ba4937deed093923
hafsa <hafsaraii>
2024-11-03 02:19:04 +0100

    add create FORM container

--- frontend/src/app/components/create-form/create-form.component.ts
+++ frontend/src/app/components/create-form/create-form.component.ts
@@ -139,8 +139,19 @@ export class CreateFormComponent {
         this.showErrors = true;
     }
  }
+
   submitForm() {
-    this.formService.createFormContainer(this.form).subscribe(
+    const payload = {
+      title: this.form.title,
+      description: this.form.description,
+      user_email: this.form.userEmail,
+      manager_email: this.form.managerEmail,
+      reference: this.form.reference,
+      escalate: this.form.escalate,
+      reminder_delay_day: this.from.reminderDelayDay,
+      form: { questions: this.form.questions }
+    };
+    this.formService.createFormContainer(payload).subscribe(
       response => {
         this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Form created successfully' });
         setTimeout(() => {
