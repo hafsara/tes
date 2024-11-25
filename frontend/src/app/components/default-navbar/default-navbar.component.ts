af5838baa2c9178f50800601fbfb2eef292ea353
hafsa <hafsaraii>
2024-11-25 17:37:21 +0100

    test correction access control

--- frontend/src/app/components/default-navbar/default-navbar.component.ts
+++ frontend/src/app/components/default-navbar/default-navbar.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-default-navbar',
   templateUrl: './default-navbar.component.html',
-  styleUrl: './default-navbar.component.scss'
+  styleUrls: ['./default-navbar.component.scss']
 })
 export class DefaultNavbarComponent {
-logout (){
-  }
+
 }
