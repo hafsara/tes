0e93d15f8caf05127e80ae999f30a5ae3f229777
hafsa <hafsaraii>
2024-12-19 19:10:18 +0100

    ça commence à marcher

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,11 +9,11 @@ import { AuthComponent } from './components/auth/auth.component';
 import { sSOGuard } from './guards/sso.guard';
 
 const routes: Routes = [
-  { path: 'access-control', component: AccessControlComponent },
+  { path: 'access-control', component: AccessControlComponent, canActivate: [sSOGuard]},
   { path: 'auth', component: AuthComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'create-form', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'dashboard/:appName/load-form/:access_token', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [sSOGuard, AuthGuard] },
+  { path: 'create-form', component: DashboardComponent, canActivate: [sSOGuard, AuthGuard] },
+  { path: 'dashboard/:appName/load-form/:access_token', component: DashboardComponent, canActivate: [sSOGuard, AuthGuard] },
   { path: 'user-view/:access_token', component: UserViewComponent, canActivate: [sSOGuard] },
   { path: '404', component: PageNotFoundComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
